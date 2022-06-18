eb1f73a716dd3e5fc9e939cecced7dc3b17745fc
zxning <412568142@qq.com>
2022-06-18 19:49:09 +0800

    更新下目录；

---
+++ T03-springsecurity-oauth2/src/main/java/com/ning/SecurityOauth2Application.java
@@ -0,0 +1,13 @@
+package com.ning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityOauth2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecurityOauth2Application.class, args);
+    }
+
+}
